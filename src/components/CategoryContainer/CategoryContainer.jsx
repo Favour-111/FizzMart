67f950a4a18a4fefe931de7a6d051d8e6aded77d
favour <omojolaobaloluwa@gmail.com>
2025-10-04 15:55:11 +0100

    done

--- src/components/CategoryContainer/CategoryContainer.jsx
+++ src/components/CategoryContainer/CategoryContainer.jsx
@@ -9,50 +9,6 @@ import CategorySkelenton from "../CategorySkelenton/CategorySkelenton";
 import { ShopContext } from "../Context/ShopContext";
 const CategoryContainer = () => {
   const { categories, categoryLoader } = useContext(ShopContext);
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 2,
-    autoplay: true, // ✅ Enable auto-scrolling
-    autoplaySpeed: 3000, // ✅ Time between transitions (3 seconds)
-    pauseOnHover: false, // ❌ Try disabling hover pause to check if it works
-    pauseOnFocus: false, //
-    responsive: [
-      {
-        breakpoint: 1280,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 2,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 1181,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 970,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          initialSlide: 3,
-        },
-      },
-      {
-        breakpoint: 680,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          initialSlide: 2,
-        },
-      },
-    ],
-  };
 
   const navigate = useNavigate();
   return (
@@ -61,25 +17,23 @@ const CategoryContainer = () => {
       <div className="New-ProductHead">Shop by Top Categories</div>
       {!categoryLoader ? (
         <div className="Category-body-container">
-          <Slider {...settings}>
-            {categories.map((item) => {
-              return (
-                <div
-                  onClick={() => {
-                    navigate(`category-${item.name}`);
-                  }}
-                  className="category"
-                >
-                  <div className="category-item shadow-sm">
-                    <div>
-                      <img loading="lazy" src={item.image} alt="" />
-                    </div>
+          {categories.map((item) => {
+            return (
+              <div
+                onClick={() => {
+                  navigate(`category-${item.name}`);
+                }}
+                className="category"
+              >
+                <div className="category-item shadow-sm">
+                  <div>
+                    <img loading="lazy" src={item.image} alt="" />
                   </div>
-                  <div className="category-list-name">{item.name}</div>
                 </div>
-              );
-            })}
-          </Slider>
+                <div className="category-list-name">{item.name}</div>
+              </div>
+            );
+          })}
         </div>
       ) : (
         <div className="mt-3 Category-body-container d-flex align-items-center justify-content-center gap-2">
